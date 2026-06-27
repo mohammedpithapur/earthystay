b9f42011a63d6a3a8d954d7e45e86600a689fd66
mohammedpithapur <mohammedpithapur786@gmail.com>
2026-06-27 08:34:21 +0530

    removed unwanted files

--- backend/alembic/versions/20260625_01_add_max_pets.py
+++ backend/alembic/versions/20260625_01_add_max_pets.py
@@ -1,4 +1,4 @@
-﻿"""add max_pets to properties
+"""add max_pets to properties
 
 Revision ID: 20260625_01_add_max_pets
 Revises: 20260531_01_production_indexes_user_flags
@@ -9,7 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = '20260625_01_add_max_pets'
-down_revision = '20260531_01_production_indexes_user_flags'
+down_revision = 'a1b2c3d4e5f6'
 branch_labels = None
 depends_on = None
 
